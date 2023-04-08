95266e264b48ddf7a7ec032fc1e4fa8daddd57c5
William Gibson <wdr.gibson@gmail.com>
2023-04-08 00:04:35 -0500

    Completed carousel

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,15 +1,49 @@
 export default function initializeCarousel(){
     
     const carousel = document.querySelector('.games-carousel');
-    const section = document.querySelector('.game-section');
-    const right = document.querySelector('.right');
-    const left = document.querySelector('.left');    
+    const gameSection = document.querySelector('.game-section');
+    const rightButton = document.querySelector('.right');
+    const leftButton = document.querySelector('.left'); 
+
+    const carouselWidth = carousel.scrollWidth;
+    const gameSectionWidth = gameSection.clientWidth;
+
+    let scrollPosition = 0 
+
+    // Updates carousel scroll position to display each game 
+    // based on arrow click or key press
+    function updateScrollPosition(delta){
+        scrollPosition += delta
+        
+        if(scrollPosition < 0){
+            scrollPosition = carouselWidth - gameSectionWidth
+        }
+        else if (scrollPosition + gameSectionWidth > carouselWidth){
+            scrollPosition = 0
+        }
+
+        carousel.scrollTo({
+            left: scrollPosition,
+            behavior: 'smooth'
+        })
+    }
     
-    right.addEventListener('click', function (e) {
-        carousel.scrollLeft += section.clientWidth;
+    // Listeners for arrow button clicks and keydowns
+    rightButton.addEventListener('click', () =>  {
+        updateScrollPosition(gameSectionWidth);
     });
     
-    left.addEventListener('click', function () {
-        carousel.scrollLeft -= section.clientWidth;
+    leftButton.addEventListener('click', () => {
+        updateScrollPosition(-gameSectionWidth);
+    });
+
+    document.addEventListener('keydown', (event) => {
+        event.preventDefault()
+        if (event.code == 'ArrowRight'){
+            updateScrollPosition(gameSectionWidth);
+        }
+        else if (event.code == 'ArrowLeft'){
+            updateScrollPosition(-gameSectionWidth);
+        }
     });
 }
