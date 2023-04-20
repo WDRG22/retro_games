2ca527777fc6780922d6f63da1c7bd8dfced9ae5
Will <wdr.gibson@gmail.com>
2023-04-20 02:31:26 -0500

    after adding mongoDB

--- server/app.js
+++ server/app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
-const connectDB = require('../config/db');
+const connectDB = require("../config/db");
 
 // connect to db
 connectDB();
 
 // middleware
 app.use((req, res, next) => {
-	console.log(`Incoming request: ${req.method} ${req.url}`);
-	next();
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
 });
 
 app.use(express.static(path.join(__dirname, '../public')));
@@ -22,11 +22,11 @@ app.get('*', (req, res) => {
 
 // binds to port to listen for any connections
 const server = app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`);
+})
 
 // on unhandledRejection error close server with exit code 1
-process.on('unhandledRejection', err => {
-	console.log(`An error occurred:  ${err.message}`);
-	server.close(() => process.exit(1));
-});
+process.on("unhandledRejection", err => {
+  console.log(`An error occurred:  ${err.message}`)
+  server.close(() => process.exit(1));
+})
